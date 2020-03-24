d94585cad811ac0f55d4ce499f09b1a1f07134e7
tmeissner <programming@goodcleanfun.de>
2020-03-24 14:32:54 +0100

    Making counter design work with GHDL synthesis
    * Remove unused SVA properties file
    * Makefile optimizations
    * Use prep auto-top option to prevent error with not founded top-level module
      when using generics
    * Add some simple PSL assertions

--- counter/counter.vhd
+++ counter/counter.vhd
@@ -7,7 +7,8 @@ use ieee.numeric_std.all;
 entity counter is
   generic (
     InitVal : natural := 0;
-    EndVal  : natural := 64
+    EndVal  : natural := 16;
+    Formal  : boolean := true
   );
   port (
     Reset_n_i : in  std_logic;
@@ -34,6 +35,40 @@ begin
       end if;
     end if;
   end process;
+
+
+  FormalG : if Formal generate
+
+    signal s_data : unsigned(Data_o'range);
+
+  begin
+
+    -- VHDL helper logic
+    process is
+    begin
+      wait until rising_edge(Clk_i);
+      s_data <= unsigned(Data_o);
+    end process;
+
+    default clock is rising_edge(Clk_i);
+
+    -- Initial reset
+    INITIAL_RESET : restrict {Reset_n_i = '0'[*2]; Reset_n_i = '1'[+]}[*1];
+
+    AFTER_RESET : assert always
+      not Reset_n_i -> Data_o = (Data_o'range => '0');
+
+    COUNT_UP : assert always
+      Reset_n_i and unsigned(Data_o) < to_unsigned(EndVal, 32) -> next unsigned(Data_o) = s_data + 1;
+
+    END_VALUE : assert always
+      unsigned(Data_o) = to_unsigned(EndVal, 32) -> next unsigned(Data_o) = s_data;
+
+    VALID_RANGE : assert always
+      unsigned(Data_o) >= to_unsigned(InitVal, 32) and
+      unsigned(Data_o) <= to_unsigned(EndVal, 32);
+
+  end generate FormalG;
 
 
 end architecture rtl;
